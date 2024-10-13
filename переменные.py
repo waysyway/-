2b7307dcf1db6131e4a9743987032bff14ff1e40
waysyway <waysyway94@gmail.com>
2024-10-13 11:14:07 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,8 @@
+Number_of_completed_remote_sensing = (12)
+print(Number_of_completed_remote_sensing )
+Number_of_hours_spent = (1.5)
+print(Number_of_hours_spent)
+Course_name = 'python'
+print(Course_name)
+Time_per_task = (Number_of_completed_remote_sensing / Number_of_hours_spent)
+print(Time_per_task)
